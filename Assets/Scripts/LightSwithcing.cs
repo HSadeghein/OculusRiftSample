079d151799b8e3e123a875c9c0903a3930aa7450
Hassan-Sadeghein <mohammad.sadeghein@student.oulu.fi>
2019-11-06 12:25:16 +0200

    Initial state

---
+++ Assets/Scripts/LightSwithcing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus;
+public class LightSwithcing : MonoBehaviour
+{
+
+    private Light mLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        if(OVRInput.GetDown(OVRInput.Button.One,OVRInput.Controller.RTouch))
+        {
+            mLight.color = new Color(1.0f, 0.0f, 0.0f);
+        }
+    }
+    private void FixedUpdate()
+    {
+        OVRInput.FixedUpdate();
+    }
+}
---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus;
+public class Quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+
+    void FixedUpdate()
+    {
+        OVRInput.FixedUpdate();
+    }
+}
